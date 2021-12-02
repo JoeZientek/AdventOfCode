a3509ae8482b958feacdb34a6747b16a1c0c6b7b
Joe Zientek <jzientek@west-inc.com>
2021-12-01 22:42:16 -0700

    part B done

--- Day01/Day01/Program.cs
+++ Day01/Day01/Program.cs
@@ -8,23 +8,38 @@ namespace Day01
     {
         static void Main(string[] args)
         {
+            // part A
 
-            var input = File.ReadAllLines("input.txt");
+            var input = File.ReadAllLines("input.txt").Select(x => int.Parse(x)).ToArray(); ;
 
             var count = 0;
 
             // start comparing second measurement to first measurement
             foreach(int measurementNumber in Enumerable.Range(1, input.Length - 1))
             {
-                
-                if (Int32.Parse(input[measurementNumber]) > Int32.Parse(input[measurementNumber - 1]))
-                {
+                if (input[measurementNumber] > input[measurementNumber - 1])
                     count++;
-                }
-                
             }
 
             Console.WriteLine(count);
+
+            // part B 
+            var currentSum = input.Take(3).Sum();
+            var numIncreases = 0;
+
+            foreach(int measurementNumber in Enumerable.Range(1, input.Length - 1))
+            {
+                if(measurementNumber + 2 <= input.Length)
+                {
+                    var previousSum = currentSum;
+                    currentSum = input.Skip(measurementNumber).Take(3).Sum();
+
+                    if(currentSum > previousSum)
+                        numIncreases++;
+                }
+            }
+
+            Console.WriteLine(numIncreases);
         }
     }
 }
