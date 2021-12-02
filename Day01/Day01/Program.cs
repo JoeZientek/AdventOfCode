e21f2530635e87b89ea4bb46ca6f8678dbb70489
Joe Zientek <jzientek@west-inc.com>
2021-12-01 22:12:28 -0700

    part A done

---
+++ Day01/Day01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var input = File.ReadAllLines("input.txt");
+
+            var count = 0;
+
+            // start comparing second measurement to first measurement
+            foreach(int measurementNumber in Enumerable.Range(1, input.Length - 1))
+            {
+                
+                if (Int32.Parse(input[measurementNumber]) > Int32.Parse(input[measurementNumber - 1]))
+                {
+                    count++;
+                }
+                
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
